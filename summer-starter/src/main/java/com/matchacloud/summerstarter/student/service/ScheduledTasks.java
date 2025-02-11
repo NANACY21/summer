31d7ddf4cba7193e85163ffb4cc969d9e6f2c666
NANACY21 <2050761966@qq.com>
2025-02-11 21:26:40 +0800

    日志相关知识

--- summer-starter/src/main/java/com/matchacloud/summerstarter/student/service/ScheduledTasks.java
+++ summer-starter/src/main/java/com/matchacloud/summerstarter/student/service/ScheduledTasks.java
@@ -1,5 +1,7 @@
 package com.matchacloud.summerstarter.student.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -15,11 +17,18 @@ import org.springframework.stereotype.Component;
 @Component
 @EnableScheduling
 public class ScheduledTasks {
+    // 获取当前类对应的日志记录器
+    private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
+
     /**
      * 每 5 秒执行一次
      */
     @Scheduled(fixedRate = 5000)
     public void reportCurrentTime() {
+        // 记录不同级别的日志
+        logger.trace("进入 reportCurrentTime 方法，开始执行任务");
+        logger.debug("正在执行任务的某个步骤");
+        logger.info("任务执行完成，一切正常");
         System.out.println("当前时间：" + new java.util.Date());
     }
 }
