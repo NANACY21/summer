7fe8c4fbf1deb1d2e530b3c74a85f24c0ace33bb
NANACY21 <2050761966@qq.com>
2025-02-21 19:11:31 +0800

    Lock接口与同步关键字

--- J2SE/src/main/java/com/matchacloud/basic/thread/lock/MySynchronized.java
+++ J2SE/src/main/java/com/matchacloud/basic/thread/lock/MySynchronized.java
@@ -1,16 +1,40 @@
 package com.matchacloud.basic.thread.lock;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
- * 同步关键字:排他锁(锁同一个对象则不能同时执行) 可重入锁 非公平锁
- * 悲观锁 上锁解锁自动完成 线程发生异常自动释放锁
- *
- *
- * synchronized还可以修饰一个类
+ * 同步关键字(synchronized)介绍:
+ * 1.排他锁(也叫悲观锁，锁同一个对象则不能同时执行)、可重入锁、非公平锁
+ * 2.上锁/解锁自动完成，线程发生异常自动释放锁
+ * 3.synchronized还可以修饰一个类
+ * <p>
+ * ReentrantLock介绍:
+ * 1.排他锁(悲观锁)、可重入锁、公平锁/非公平锁
+ * 2.实现Lock接口的类需要手动加锁/释放锁，出现异常也不会自动释放锁，仍需要手动释放锁
+ * 3.ReentrantLock类(可重入锁)是Lock接口的一个实现类 该类的一个对象作为锁，一个锁 对应 多个线程
+ * <p>
+ * 同步关键字和ReentrantLock的区别：
+ * 1.Lock可让等待锁的线程响应中断(如果lock()获取锁失败会进入等待状态且不会停止等待，
+ *   但如果lockInterruptibly()方法来尝试获取锁，获取锁失败等待则有可能不会一直等待下去，
+ *   会抛异常，如果其他线程调：等待的线程.中断()则等待的线程会终止)，
+ *   而synchronized不行，使用synchronized时，
+ *   等待的线程会一直等待下去，不能够响应中断
+ * 2.通过 Lock 可以知道有没有成功获取锁(根据lock()返回值)，而synchronized 却无法办到
+ * 3.而当竞争资源非常激烈时(即有大量线程同时竞争)，此时 Lock的性能要远远优于synchronized
+ *   因为使用lock接口可以中断等待的线程，
+ *   lock接口，如果超时未获取到锁会执行其他逻辑，这是同步关键字办不到的
+ *   还有公平锁/非公平锁机制
+ *   lock接口还可以通过newCondition()进行精确的线程间通信
  */
-public class SyncTest {
+public class MySynchronized {
 
     private static final Object lock = new Object();
     private int a;
+
+    /**
+     *
+     */
+    ReentrantLock reentrantLock = new ReentrantLock();
 
 
     public static void a() {
@@ -41,7 +65,7 @@ public class SyncTest {
     /**
      * synchronized作用在实例方法，锁的是当前对象，
      * 该方法得要到锁并锁上当前对象才执行
-     *
+     * <p>
      * 修饰一个方法:一个线程执行该方法时 获取锁 加锁 执行 别的线程获取锁失败阻塞 这时 锁 是方法所在类的实例对象
      * 如果创建了两个对象 两个线程分别调这两个对象的同步方法 则俩线程会同时执行方法!!!因为俩线程不是同一把锁!!!
      */
@@ -78,20 +102,20 @@ public class SyncTest {
 
     public static void main(String[] args) {
 
-        SyncTest test = new SyncTest();
+        MySynchronized test = new MySynchronized();
 
         //同步锁示例
         new Thread(new Runnable() {
             @Override
             public void run() {
-                SyncTest.a();
+                MySynchronized.a();
             }
         }, "T1").run();
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                SyncTest.b();
+                MySynchronized.b();
             }
         }, "T2").run();
 
@@ -105,7 +129,7 @@ public class SyncTest {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                SyncTest.d();
+                MySynchronized.d();
             }
         }, "T4").start();
         new Thread(new Runnable() {
